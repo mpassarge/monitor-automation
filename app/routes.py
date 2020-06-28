3e281e61794e8ab629d5cf23c2083be5b5f2f9b7
Michael Passarge <mpassarge@comcast.net>
2020-06-27 21:17:48 -0400

    Added POC of running bash command after api call

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from app import app
+from subprocess import Popen,PIPE
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    process = Popen(['ls', '.'], stdout=PIPE)
+    output, error = process.communicate()
+    return output
