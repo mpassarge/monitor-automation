c1ce58f74ff48d1b3b7864f36bdfb82c2d7ede22
Michael Passarge <mpassarge@comcast.net>
2020-07-13 20:07:01 -0400

    Added monitor-ws to port 3001

--- monitor-ws/src/index.js
+++ monitor-ws/src/index.js
@@ -8,7 +8,7 @@ app.use(morgan('common'));
 app.use(express.json());
 app.use('/monitor', routes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
